f35050cf543c954711e27f8d42744668ad9f5189
Leighton Guang <leighton.guang@icloud.com>
2024-05-01 15:40:19 +0100

    feat: setting page

---
+++ app/setting/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SettingPage = () => {
+  return (
+    <div className="m-4">
+      <h1>Setting</h1>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default SettingPage;

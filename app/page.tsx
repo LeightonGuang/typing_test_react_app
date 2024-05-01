8a32046ed7ed27acb87da20546b026f2e434d2b9
Leighton Guang <leighton.guang@icloud.com>
2024-05-01 15:40:50 +0100

    feat: type homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="m-4">
       <div>
         <h1>Home</h1>
-        <input type="text" />
+        <input type="text" className="outline" />
       </div>
     </main>
   );

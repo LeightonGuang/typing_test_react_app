721b69a62a0e2e3f628ed0a6c72ef73227e5b646
Leighton Guang <leighton.guang@icloud.com>
2024-05-09 23:03:20 +0100

    feat: login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogInPage = () => {
+  return (
+    <div className="m-4">
+      <h1>Log In</h1>
+      <button>Google</button>
+    </div>
+  );
+};
+
+export default LogInPage;

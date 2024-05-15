c160dc76921a761405d15fc9cad759774dde88b4
Leighton Guang <leighton.guang@icloud.com>
2024-05-15 03:17:30 +0100

    feat: add styling to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import TypingArea from "@/components/TypingArea";
 
 export default function Home() {
   return (
-    <main className="m-4">
-      <div>
-        <h1>Home</h1>
-        <input type="text" className="outline" />
-        <TypingArea />
+    <main className="home flex h-dvh w-dvw">
+      <div className="home__container justify-center my-48 mx-auto">
+        {/* <h1 className="home__title">Home</h1> */}
+        <div className="">
+          <TypingArea />
+        </div>
       </div>
     </main>
   );

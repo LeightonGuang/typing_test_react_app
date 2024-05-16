48abaaf845c0d33abe4c5991810c37b15e8ece4d
Leighton Guang <leighton.guang@icloud.com>
2024-05-16 18:06:52 +0100

    feat: add blur when text area is not focused

--- components/TypingArea.tsx
+++ components/TypingArea.tsx
@@ -1,30 +1,45 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import Word from "./Word";
 
 const TypingArea = () => {
-  const [key, setKey] = useState<string>("");
+  const [key, setKey]: [string, any] = useState<string>("");
+  const divRef: any = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocused] = useState<boolean>(true);
   const wordList: string[] = ["typing", "area", "one", "two", "three"];
+
+  const checkDivFocus: any = () => {
+    if (divRef.current === document.activeElement) {
+      setIsFocused(true);
+    } else if (divRef.current !== document.activeElement) {
+      setIsFocused(false);
+    }
+  };
 
   const handleKeyPress = (event: any) => {
     setKey(event.key);
   };
 
   return (
-    <>
+    <div>
       <div>{key}</div>
       <div
         onKeyDown={handleKeyPress}
+        ref={divRef}
         tabIndex={0}
-        className=" inline-flex gap-2 bg-slate-600 p-3 rounded-md text-[1.2rem]"
+        onFocus={checkDivFocus}
+        onBlur={checkDivFocus}
+        className={`inline-flex gap-2 bg-slate-600 p-3 rounded-md text-[1.2rem] ${
+          isFocused ? "" : "filter blur-sm"
+        }`}
       >
         {wordList.map((word) => (
           <Word word={word} />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 

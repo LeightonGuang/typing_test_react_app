f2c2673fba098b50c51c40aa7cf86f899cef2624
Leighton Guang <leighton.guang@icloud.com>
2024-05-16 17:31:32 +0100

    feat: split words into individual characters

--- components/Word.tsx
+++ components/Word.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Word = ({ word }: { word: string }) => {
-  return <div>{word}</div>;
+  const splitedWord = word.split("");
+  return (
+    <div className="word">
+      {splitedWord.map((char, index) => (
+        <span key={index} className="word__char text-white">
+          {char}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Word;

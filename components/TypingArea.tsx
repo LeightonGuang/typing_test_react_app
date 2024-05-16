b362b406da755a8d210201238e1ac30811a6ef65
Leighton Guang <leighton.guang@icloud.com>
2024-05-16 17:31:57 +0100

    feat: show what key is pressed

--- components/TypingArea.tsx
+++ components/TypingArea.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import Word from "./Word";
 
 const TypingArea = () => {
+  const [key, setKey] = useState<string>("");
+  const wordList: string[] = ["typing", "area", "one", "two", "three"];
+
+  const handleKeyPress = (event: any) => {
+    setKey(event.key);
+  };
+
   return (
-    <div>
-      <Word word="Typing" />
-      <Word word="Area" />
-    </div>
+    <>
+      <div>{key}</div>
+      <div
+        onKeyDown={handleKeyPress}
+        tabIndex={0}
+        className=" inline-flex gap-2 bg-slate-600 p-3 rounded-md text-[1.2rem]"
+      >
+        {wordList.map((word) => (
+          <Word word={word} />
+        ))}
+      </div>
+    </>
   );
 };
 

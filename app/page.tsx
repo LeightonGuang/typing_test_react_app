d78824e32d7ebad04ce250839cca1891a2b2d514
Leighton Guang <leighton.guang@icloud.com>
2024-05-19 23:36:42 +0100

    refactor: remove not needed elements

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className="home flex h-dvh w-dvw">
       <div className="home__container justify-center my-48 mx-auto">
-        {/* <h1 className="home__title">Home</h1> */}
-        <div className="">
-          <TypingArea />
-        </div>
+        <TypingArea />
       </div>
     </main>
   );

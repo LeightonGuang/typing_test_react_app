6e518221ca8ee3190215be4ba461171eeb3db708
Leighton Guang <leighton.guang@icloud.com>
2024-05-19 23:37:10 +0100

    feat: use char in word component

--- components/Word.tsx
+++ components/Word.tsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Char from "./Char";
 
 const Word = ({ word }: { word: string }) => {
   const splitedWord = word.split("");
   return (
     <div className="word">
       {splitedWord.map((char, index) => (
-        <span key={index} className="word__char text-white">
-          {char}
-        </span>
+        <Char key={index} char={char} isCorrect={true} />
       ))}
     </div>
   );

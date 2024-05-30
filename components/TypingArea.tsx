1eda87bf91d340a408503c03cf671b3d79b86a01
Leighton Guang <leighton.guang@icloud.com>
2024-05-30 11:43:09 +0100

    feat: import all types and word list

--- components/TypingArea.tsx
+++ components/TypingArea.tsx
@@ -4,124 +4,16 @@ import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import Char from "./Char";
 
+import { LetterType } from "@/_types/LetterType";
+import { WordIndexType } from "@/_types/WordIndexType";
+import { wordList } from "@/_assets/wordList";
+import { GenerateWordsType } from "@/_types/GenerateWordsType";
+import { ConvertWordsToDisplayWordsType } from "@/_types/ConvertWordsToDisplayWordsType";
+import { HandleKeyPressType } from "@/_types/HandleKeyPressType";
+
 type TypingAreaType = () => JSX.Element;
 
 const TypingArea: TypingAreaType = () => {
-  interface LetterType {
-    char: string;
-    typedChar: string | null;
-    isCorrect: boolean | null;
-  }
-
-  interface WordIndexType {
-    index: number;
-    letterIndex: number;
-  }
-
-  const wordList: string[] = [
-    "the",
-    "at",
-    "there",
-    "some",
-    "my",
-    "of",
-    "be",
-    "use",
-    "her",
-    "than",
-    "and",
-    "this",
-    "an",
-    "would",
-    "first",
-    "a",
-    "have",
-    "each",
-    "make",
-    "water",
-    "to",
-    "from",
-    "which",
-    "like",
-    "been",
-    "in",
-    "or",
-    "she",
-    "him",
-    "call",
-    "is",
-    "one",
-    "do",
-    "into",
-    "who",
-    "you",
-    "had",
-    "how",
-    "time",
-    "oil",
-    "that",
-    "by",
-    "their",
-    "has",
-    "its",
-    "it",
-    "word",
-    "if",
-    "look",
-    "now",
-    "he",
-    "but",
-    "will",
-    "two",
-    "find",
-    "was",
-    "not",
-    "up",
-    "more",
-    "long",
-    "for",
-    "what",
-    "other",
-    "write",
-    "down",
-    "on",
-    "all",
-    "about",
-    "go",
-    "day",
-    "are",
-    "were",
-    "out",
-    "see",
-    "did",
-    "as",
-    "we",
-    "many",
-    "number",
-    "get",
-    "with",
-    "when",
-    "then",
-    "no",
-    "come",
-    "his",
-    "your",
-    "them",
-    "way",
-    "made",
-    "they",
-    "can",
-    "these",
-    "could",
-    "may",
-    "I",
-    "said",
-    "so",
-    "people",
-    "part",
-  ];
-
-  const [key, setKey]: [string, any] = useState<string>("");
   const typingAreaRef: any = useRef<HTMLDivElement>(null);
   const [isFocused, setIsFocused] = useState<boolean>(true);
   const [wordIndex, setwordIndex] = useState<WordIndexType>({
@@ -138,8 +30,6 @@ const TypingArea: TypingAreaType = () => {
    * ]
    */
 
-  type GenerateWordsType = () => string[];
-
   const generateWords: GenerateWordsType = (numWords: number = 10) => {
     const newWordList: string[] = [];
     for (let i: number = 0; i < numWords; i++) {
@@ -149,8 +39,6 @@ const TypingArea: TypingAreaType = () => {
     }
     return newWordList;
   };
-
-  type ConvertWordsToDisplayWordsType = (words: string[]) => Object[][];
 
   // convert a list of words to a list of display words with <span> around every letter
   const convertWordsToDisplayWords: ConvertWordsToDisplayWordsType = (
@@ -176,11 +64,8 @@ const TypingArea: TypingAreaType = () => {
     }
   };
 
-  type HandleKeyPressType = (event: any) => void;
-
   const handleKeyPress: HandleKeyPressType = (event) => {
     const typedKey: string = event.key;
-    setKey(typedKey);
 
     console.log(displayWordList);
 
@@ -293,7 +178,6 @@ const TypingArea: TypingAreaType = () => {
 
   return (
     <div className="typingArea ">
-      <div>{key}</div>
       <div
         onKeyDown={handleKeyPress}
         ref={typingAreaRef}

d2bcb67c4cc661baa898135de7c394d666478191
Leighton Guang <leighton.guang@icloud.com>
2024-06-05 18:01:26 +0100

    feat: new typing stats component

---
+++ components/TypingStats.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TypingStats = () => {
+  return (
+    <div className="typingStats">
+      <div className="typingStats__container">
+        <div className="typingStats__wpm">WPM</div>
+      </div>
+    </div>
+  );
+};
+
+export default TypingStats;

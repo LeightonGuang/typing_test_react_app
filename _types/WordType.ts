30dbd628aeb9034cad34562ada7c5cac65bba84f
Leighton Guang <leighton.guang@icloud.com>
2024-06-11 13:51:46 +0100

    refactor: change word list structure

---
+++ _types/WordType.ts
@@ -0,0 +1,5 @@
+import { LetterType } from "./LetterType";
+
+export interface WordType {
+  wordLetters: LetterType[];
+}
--- components/TypingAreaBlock.tsx
+++ components/TypingAreaBlock.tsx
@@ -5,12 +5,13 @@ import { useState } from "react";
 import Word from "./Word";
 
 import { TypingAreaType } from "@/_types/TypingAreaType";
-import { LetterType } from "@/_types/LetterType";
 import { WordIndexType } from "@/_types/WordIndexType";
-import { wordList } from "@/_assets/wordList";
+import { testWordList } from "@/_assets/testWordList";
 import { GenerateWordsType } from "@/_types/GenerateWordsType";
-import { ConvertWordsToDisplayWordsType } from "@/_types/ConvertWordsToDisplayWordsType";
+import { ConvertWordsToDisplayWordsListType } from "@/_types/ConvertWordsToDisplayWordsListType";
 import { HandleKeyPressType } from "@/_types/HandleKeyPressType";
+import { LetterType } from "@/_types/LetterType";
+import { WordType } from "@/_types/WordType";
 
 const TypingAreaBlock: TypingAreaType = () => {
   const typingAreaRef: any = useRef<HTMLDivElement>(null);
@@ -20,56 +21,62 @@ const TypingAreaBlock: TypingAreaType = () => {
     letterIndex: -1,
   });
 
-  const [lastTwoTypedWords, setLastTwoTypedWords] = useState<LetterType[][]>([
-    [{ char: null, typedChar: null, isCorrect: null }],
-    [{ char: null, typedChar: null, isCorrect: null }],
+  const [lastTwoTypedWordsList, setLastTwoTypedWordsList] = useState<
+    WordType[]
+  >([
+    { wordLetters: [{ char: null, typedChar: null, isCorrect: null }] },
+    { wordLetters: [{ char: null, typedChar: null, isCorrect: null }] },
   ]);
 
-  const [displayWordList, setDisplayWordList] = useState<LetterType[][]>([]);
+  const [displayWordList, setDisplayWordList] = useState<WordType[]>([]);
 
-  /*
-  displayWordList: [
-    [
-      { char: "h", typedChar: null, isCorrect: null },
-      { char: "e", typedChar: null, isCorrect: null },
-      { char: "l", typedChar: null, isCorrect: null },
-      { char: "l", typedChar: null, isCorrect: null },
-      { char: "o", typedChar: null, isCorrect: null },
-    ],
-    [
-      { char: "w", typedChar: null, isCorrect: null },
-      { char: "o", typedChar: null, isCorrect: null },
-      { char: "r", typedChar: null, isCorrect: null },
-      { char: "l", typedChar: null, isCorrect: null },
-      { char: "d", typedChar: null, isCorrect: null },
-    ],
-  ],
- */
+  // const displayWordList: WordType[] = [
+  //   {
+  //     wordLetters: [
+  //       { char: "h", typedChar: null, isCorrect: null },
+  //       { char: "e", typedChar: null, isCorrect: null },
+  //       { char: "l", typedChar: null, isCorrect: null },
+  //       { char: "l", typedChar: null, isCorrect: null },
+  //       { char: "o", typedChar: null, isCorrect: null },
+  //     ],
+  //   },
+  //   {
+  //     wordLetters: [
+  //       [
+  //         { char: "w", typedChar: null, isCorrect: null },
+  //         { char: "o", typedChar: null, isCorrect: null },
+  //         { char: "r", typedChar: null, isCorrect: null },
+  //         { char: "l", typedChar: null, isCorrect: null },
+  //         { char: "d", typedChar: null, isCorrect: null },
+  //       ],
+  //     ],
+  //   },
+  // ];
 
   const generateWords: GenerateWordsType = (numWords: number = 10) => {
     const newWordList: string[] = [];
     for (let i: number = 0; i < numWords; i++) {
       const newWord: string =
-        wordList[Math.floor(Math.random() * wordList.length)];
+        testWordList[Math.floor(Math.random() * testWordList.length)];
       newWordList.push(newWord);
     }
     return newWordList;
   };
 
   // convert a list of words to a list of display words with <span> around every letter
-  const convertWordsToDisplayWords: ConvertWordsToDisplayWordsType = (
+  const convertWordsToDisplayWordsList: ConvertWordsToDisplayWordsListType = (
     words: string[]
   ) => {
-    const groupedList: Object[][] = words.map((word: string) => {
+    const groupedWordsList: WordType[] = words.map((word: string) => {
       const splitedWord: string[] = word.split("");
-      const wrapedWord: Object[] = splitedWord.map((char: string) => ({
+      const wrapedWord: LetterType[] = splitedWord.map((char: string) => ({
         char: char,
         typedChar: null,
         isCorrect: null,
       }));
-      return wrapedWord;
+      return { wordLetters: wrapedWord };
     });
-    return groupedList;
+    return groupedWordsList;
   };
 
   const checkDivFocus: any = () => {
@@ -80,52 +87,62 @@ const TypingAreaBlock: TypingAreaType = () => {
     }
   };
 
-  function handleSpaceKeyPress(): void {
-    const newTypedList: LetterType[][] = [];
-    [newTypedList[0], newTypedList[1]] = [
-      lastTwoTypedWords[1],
-      displayWordList[wordIndex.index],
-    ];
-    setLastTwoTypedWords(newTypedList);
-    setwordIndex({ index: wordIndex.index + 1, letterIndex: -1 });
-  }
-
-  function handleBackSpaceKeyPress(): void {
-    if (wordIndex.index === 0 && wordIndex.letterIndex === -1) return;
-    // first letter of the word
-
-    console.log({
-      index: wordIndex.index,
-      letterIndex: wordIndex.letterIndex,
-    });
-
-    // remove letter from typed list
-    const newDisplayWordList: LetterType[][] = [...displayWordList];
-
-    newDisplayWordList[wordIndex.index][wordIndex.letterIndex] = {
-      ...newDisplayWordList[wordIndex.index][wordIndex.letterIndex],
-      typedChar: "",
-      isCorrect: null,
-    };
-
-    setDisplayWordList(newDisplayWordList);
-
-    if (wordIndex.letterIndex === -1) {
-      // first letter of the word
-      setwordIndex({
-        index: wordIndex.index - 1,
-        letterIndex: displayWordList[wordIndex.index - 1].length - 1,
-      });
-    } else if (wordIndex.letterIndex !== -1) {
-      // not the first letter of the word
-      setwordIndex({
-        index: wordIndex.index,
-        letterIndex: wordIndex.letterIndex - 1,
-      });
-    }
-  }
-
   const handleKeyPress: HandleKeyPressType = (event) => {
+    function handleBackSpaceKeyPress(): void {
+      if (wordIndex.index === 0 && wordIndex.letterIndex === -1) return;
+      // first letter of the word
+
+      console.log({
+        index: wordIndex.index,
+        letterIndex: wordIndex.letterIndex,
+      });
+
+      const newTypedList: WordType[] = [];
+      [newTypedList[0], newTypedList[1]] = [
+        displayWordList[wordIndex.index - 2],
+        displayWordList[wordIndex.index - 1],
+      ];
+
+      // remove letter from typed list
+      let newDisplayWordList: WordType[] = [];
+      newDisplayWordList = [...displayWordList];
+
+      newDisplayWordList[wordIndex.index].wordLetters[wordIndex.letterIndex] = {
+        ...newDisplayWordList[wordIndex.index].wordLetters[
+          wordIndex.letterIndex
+        ],
+        typedChar: "",
+        isCorrect: null,
+      };
+
+      setDisplayWordList(newDisplayWordList);
+
+      if (wordIndex.letterIndex === -1) {
+        // first letter of the word
+        setwordIndex({
+          index: wordIndex.index - 1,
+          letterIndex:
+            displayWordList[wordIndex.index - 1].wordLetters.length - 1,
+        });
+      } else if (wordIndex.letterIndex !== -1) {
+        // not the first letter of the word
+        setwordIndex({
+          index: wordIndex.index,
+          letterIndex: wordIndex.letterIndex - 1,
+        });
+      }
+    }
+
+    function handleSpaceKeyPress(): void {
+      const newTypedList: WordType[] = [];
+      [newTypedList[0], newTypedList[1]] = [
+        lastTwoTypedWordsList[1],
+        displayWordList[wordIndex.index],
+      ];
+      setLastTwoTypedWordsList(newTypedList);
+      setwordIndex({ index: wordIndex.index + 1, letterIndex: -1 });
+    }
+
     const typedKey: string = event.key;
 
     console.log(displayWordList);
@@ -133,7 +150,6 @@ const TypingAreaBlock: TypingAreaType = () => {
     if (typedKey === "Backspace") {
       handleBackSpaceKeyPress();
     } else if (typedKey === " ") {
-      // space
       handleSpaceKeyPress();
     } else if (/[a-zA-Z]/.test(typedKey)) {
       // are letters
@@ -156,13 +172,13 @@ const TypingAreaBlock: TypingAreaType = () => {
       }
 
       const letterObj: LetterType | undefined =
-        displayWordList[wordIndex.index]?.[newLetterIndex];
+        displayWordList[wordIndex.index]?.wordLetters[newLetterIndex];
 
       // add letter to typed list
-      setDisplayWordList((prevDisplayWordList: LetterType[][]) => {
-        const newDisplayWordList: LetterType[][] = [...prevDisplayWordList];
-        newDisplayWordList[wordIndex.index][newLetterIndex] = {
-          ...newDisplayWordList[wordIndex.index][newLetterIndex],
+      setDisplayWordList((prevDisplayWordList: WordType[]) => {
+        const newDisplayWordList: WordType[] = [...prevDisplayWordList];
+        newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex] = {
+          ...newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex],
           typedChar: typedKey,
         };
         return newDisplayWordList;
@@ -172,19 +188,19 @@ const TypingAreaBlock: TypingAreaType = () => {
 
       // determine if letter is correct or not
       if (letterObj.char === typedKey) {
-        setDisplayWordList((prevDisplayWordList: LetterType[][]) => {
-          const newDisplayWordList: LetterType[][] = [...prevDisplayWordList];
-          newDisplayWordList[wordIndex.index][newLetterIndex] = {
-            ...newDisplayWordList[wordIndex.index][newLetterIndex],
+        setDisplayWordList((prevDisplayWordList: WordType[]) => {
+          const newDisplayWordList: WordType[] = [...prevDisplayWordList];
+          newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex] = {
+            ...newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex],
             isCorrect: true,
           };
           return newDisplayWordList;
         });
       } else if (letterObj.char !== typedKey) {
-        setDisplayWordList((prevDisplayWordList: LetterType[][]) => {
-          const newDisplayWordList: LetterType[][] = [...prevDisplayWordList];
-          newDisplayWordList[wordIndex.index][newLetterIndex] = {
-            ...newDisplayWordList[wordIndex.index][newLetterIndex],
+        setDisplayWordList((prevDisplayWordList: WordType[]) => {
+          const newDisplayWordList: WordType[] = [...prevDisplayWordList];
+          newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex] = {
+            ...newDisplayWordList[wordIndex.index].wordLetters[newLetterIndex],
             isCorrect: false,
           };
           return newDisplayWordList;
@@ -195,22 +211,26 @@ const TypingAreaBlock: TypingAreaType = () => {
     }
   };
 
-  const typingAreaWords: () => LetterType[][] = () => {
-    const wordsList: LetterType[][] = [];
+  const typingAreaWords: () => WordType[] = () => {
+    const wordObjList: WordType[] = [];
 
     for (let i: number = 0; i < 15; i++) {
-      wordsList.push(displayWordList[wordIndex.index + i]);
+      if (wordIndex.index + i < displayWordList.length) {
+        wordObjList.push({
+          wordLetters: displayWordList[wordIndex.index + i].wordLetters,
+        });
+      }
     }
-    return wordsList;
+    return wordObjList;
   };
 
   useEffect(() => {
     // focus on typing area when loaded
     typingAreaRef.current.focus();
-    const newWords: string[] = generateWords();
+    const newGeneratedWords: string[] = generateWords();
     setDisplayWordList((prevArray: any) => [
       ...prevArray,
-      ...convertWordsToDisplayWords(newWords),
+      ...convertWordsToDisplayWordsList(newGeneratedWords),
     ]);
   }, []);
 
@@ -226,13 +246,23 @@ const TypingAreaBlock: TypingAreaType = () => {
       }`}
     >
       <div className="typingAreaBlock__typed flex gap-[0.1rem] flex-col text-[1.5rem] min-w-[4rem] min-h-[4.5rem]">
-        {lastTwoTypedWords.map((word: LetterType[], wIndex: number) => (
-          <Word word={word} wIndex={wIndex} isTyping={false} key={wIndex} />
+        {lastTwoTypedWordsList.map((wordObj: WordType, wIndex: number) => (
+          <Word
+            word={wordObj.wordLetters}
+            wIndex={wIndex}
+            isTyping={false}
+            key={wIndex}
+          />
         ))}
       </div>
       <div className="typingAreaBlock__column flex gap-[0.1rem] flex-col text-[1.5rem] min-w-[4rem] min-h-[18rem]">
-        {typingAreaWords().map((word: LetterType[], wIndex: number) => (
-          <Word word={word} wIndex={wIndex} isTyping={true} key={wIndex} />
+        {typingAreaWords().map((wordObj: WordType, wIndex: number) => (
+          <Word
+            word={wordObj.wordLetters}
+            wIndex={wIndex}
+            isTyping={true}
+            key={wIndex}
+          />
         ))}
       </div>
     </div>

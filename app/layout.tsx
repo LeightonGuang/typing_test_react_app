52fa8f9faa64a7685c0652a5dceea671ad1c60e8
LeightonGuang <leighton.guang@icloud.com>
2025-02-07 20:05:05 +0000

    Refactor: Remove navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
-// components
-import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +25,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
           {children}
         </ThemeProvider>
       </body>

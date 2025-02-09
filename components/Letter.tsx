eae36c4a78cd83345570b6c344cb99a22bad4458
LeightonGuang <leighton.guang@icloud.com>
2025-02-09 22:10:25 +0000

    Feat: Changing type from boolean | null to boolean | undefind

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+"use client";
 
 interface Props {
   char: string;
-  isCorrect: boolean | null;
+  isCorrect: boolean | undefined;
   isActive: boolean;
 }
 
 const Letter = ({ char, isCorrect, isActive }: Props) => {
   return (
     <span
-      className={`${isActive && "bg-slate-500 bg-opacity-100"} ${isCorrect === null && "text-white"} ${isCorrect === true && "text-green-500"} ${isCorrect === false && "text-red-500"} `}
+      className={`${isActive && "bg-slate-500 bg-opacity-100"} ${isCorrect === undefined && "text-white"} ${isCorrect === true && "text-green-500"} ${isCorrect === false && "text-red-500"} `}
     >
       {char}
     </span>

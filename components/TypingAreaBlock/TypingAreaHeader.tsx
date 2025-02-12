102b8454371f9dc8e6e794666ac151748ea2959d
LeightonGuang <leighton.guang@icloud.com>
2025-02-12 12:37:37 +0000

    Feat: Add buttons to select number of words to type

--- components/TypingAreaBlock/TypingAreaHeader.tsx
+++ components/TypingAreaBlock/TypingAreaHeader.tsx
@@ -1,27 +1,57 @@
+import { Button } from "../ui/button";
 import { CardHeader } from "../ui/card";
 
 interface Props {
   timer: number;
   wpm: number;
+  setNumWords: (numWords: number) => void;
 }
 
-export const TypingAreaHeader = ({ timer, wpm }: Props) => {
+export const TypingAreaHeader = ({ timer, wpm, setNumWords }: Props) => {
   return (
     <CardHeader>
-      <div className="flex justify-between">
-        <div className="flex flex-col justify-center gap-1">
-          <span>Time</span>
-          <span className="text-2xl font-bold">{timer}s</span>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-center gap-1">
+            <span>Time</span>
+            <span className="text-2xl font-bold">{timer}s</span>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <span>WPM</span>
+            <span className="text-2xl font-bold">{wpm}</span>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <span>Correct | Wrong</span>
+            <span className="text-2xl font-bold">0 | 0</span>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <span>WPM</span>
-          <span className="text-2xl font-bold">{wpm}</span>
-        </div>
+        <div className="flex gap-2">
+          <Button
+            className="w-full rounded-none"
+            variant={"outline"}
+            onClick={() => setNumWords(25)}
+          >
+            <span>25 words</span>
+          </Button>
 
-        <div className="flex flex-col gap-1">
-          <span>Correct | Wrong</span>
-          <span className="text-2xl font-bold">0 | 0</span>
+          <Button
+            className="w-full rounded-none"
+            variant={"outline"}
+            onClick={() => setNumWords(50)}
+          >
+            <span>50 words</span>
+          </Button>
+
+          <Button
+            className="w-full rounded-none"
+            variant={"outline"}
+            onClick={() => setNumWords(100)}
+          >
+            <span>100 words</span>
+          </Button>
         </div>
       </div>
     </CardHeader>

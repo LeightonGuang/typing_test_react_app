1fc5e14ee240057fefe53f0434546331eecd09df
LeightonGuang <leighton.guang@icloud.com>
2025-02-14 01:16:36 +0000

    Feat: Blur text when it is not in focus

--- components/TypingAreaBlock/TypingAreaContent.tsx
+++ components/TypingAreaBlock/TypingAreaContent.tsx
@@ -1,29 +1,45 @@
-import { useRef } from "react";
 import Word from "../Word";
+import { useEffect, useRef } from "react";
+
 import { TypedWordsType } from "@/_types/TypedWordsType";
 
 interface Props {
   typedWords: TypedWordsType;
   activeCharIndex: number;
   activeWordIndex: number;
+  generatedWords: string[];
+  isFocused: boolean;
+  setIsFocused: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const TypingAreaContent = ({
   typedWords,
   activeCharIndex,
   activeWordIndex,
+  generatedWords,
+  isFocused,
+  setIsFocused,
 }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onInputFocus = () => {
     inputRef.current?.focus();
-    console.log("focus");
+    setIsFocused(true);
   };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+    setIsFocused(true);
+  }, [generatedWords]);
 
   return (
     <div
-      className="m-4 flex select-none flex-wrap gap-1 overflow-hidden text-lg"
+      tabIndex={0}
+      className={`${isFocused ? null : "blur-sm"} m-4 flex select-none flex-wrap gap-1 overflow-hidden text-lg focus:outline-none`}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
       onClick={onInputFocus}
+      ref={inputRef}
     >
       {typedWords.map(({ word, typed }, wordIndex) => {
         const splittedWord = word.split("");

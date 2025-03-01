5fc8d98f832fea4281956cc75eab93f65505a99f
LeightonGuang <leighton.guang@icloud.com>
2025-03-01 04:22:33 +0000

    Feat: New setting page

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+"use client";
+
+import { useEffect } from "react";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardContent,
+} from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import { SettingIconSvg } from "@/components/icons";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 const SettingPage = () => {
+  useEffect(() => {}, []);
+
+  const { setTheme } = useTheme();
+
+  const themeButtons = [
+    { label: "Light", value: "light" },
+    { label: "Dark", value: "dark" },
+    { label: "Red Light", value: "red-light" },
+    { label: "Red Dark", value: "red-dark" },
+  ];
+
+  const handleThemeButton = (theme: string) => {
+    setTheme(theme);
+    window.location.reload();
+  };
+
   return (
-    <div className="m-4">
-      <h1>Setting</h1>
+    <div className="flex h-dvh w-dvw items-center justify-center">
+      <Card className="w-4/6 rounded-none">
+        <CardHeader>
+          <div className="flex items-center gap-4">
+            <SettingIconSvg className="h-6 w-6" />
+            <h1 className="text-2xl">Setting</h1>
+          </div>
+        </CardHeader>
+
+        <CardContent>
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full"
+            defaultValue="theme"
+          >
+            <AccordionItem value="theme" defaultChecked={true}>
+              <AccordionTrigger>
+                <span className="text-xl">Theme</span>
+              </AccordionTrigger>
+
+              <AccordionContent>
+                <div className="text-lg">
+                  <div className="flex w-full gap-2">
+                    {themeButtons.map((button) => (
+                      <Button
+                        key={button.value}
+                        onClick={() => handleThemeButton(button.value)}
+                        className={`w-full rounded-sm ${button.value}`}
+                      >
+                        {button.label}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </CardContent>
+
+        <CardFooter></CardFooter>
+      </Card>
     </div>
   );
 };

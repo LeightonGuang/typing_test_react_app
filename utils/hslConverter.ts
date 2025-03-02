a54c28bacd15c7befcd859f49245dc43ca497865
LeightonGuang <leighton.guang@icloud.com>
2025-03-02 00:36:51 +0000

    Feat: Functions to convert hsl colours

---
+++ utils/hslConverter.ts
@@ -0,0 +1,26 @@
+export const hslToHex = (hsl: string) => {
+  // Converts an HSL color value to its corresponding HEX value
+  let [h, s, l] = (hsl.match(/\d+(?:\.\d+)?/g) ?? [])?.map(Number);
+
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+};
+
+export const cssToHsl = (cssClass: string) => {
+  console.log(
+    window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue(cssClass),
+  );
+  return window
+    .getComputedStyle(document.documentElement)
+    .getPropertyValue(cssClass);
+};

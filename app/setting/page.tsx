1d007d889f33cf6885f3c1a88f0de8fb2b576006
LeightonGuang <leighton.guang@icloud.com>
2025-03-04 19:28:55 +0000

    Feat: New setting section for customising input caret

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -10,39 +10,83 @@ import {
 import {
   Accordion,
   AccordionItem,
-  AccordionContent,
   AccordionTrigger,
+  AccordionContent,
 } from "@/components/ui/accordion";
-
-import { SettingIconSvg } from "@/components/icons";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
+import { SettingIconSvg } from "@/components/icons";
+import { SettingsType } from "@/_types/SettingsType";
+
+const themeButtons = [
+  { label: "Light", value: "light" },
+  { label: "Dark", value: "dark" },
+  { label: "Red Light", value: "red-light" },
+  { label: "Red Dark", value: "red-dark" },
+  { label: "Blue Light", value: "blue-light" },
+  { label: "Blue Dark", value: "blue-dark" },
+  { label: "Orange Light", value: "orange-light" },
+  { label: "Orange Dark", value: "orange-dark" },
+];
+
 const SettingPage = () => {
   const { setTheme } = useTheme();
-
+  const [versionNumber, setVersionNumber] = useState<string>("");
+  const [localSettings, setLocalSettings] = useState<SettingsType>(
+    {} as SettingsType,
+  );
   const [localTheme, setLocalTheme] = useState<string>("dark");
-
-  useEffect(() => {
-    const localStorageTheme = localStorage.getItem("theme");
-    setLocalTheme(localStorageTheme!);
-  }, []);
-
-  const themeButtons = [
-    { label: "Light", value: "light" },
-    { label: "Dark", value: "dark" },
-    { label: "Red Light", value: "red-light" },
-    { label: "Red Dark", value: "red-dark" },
-    { label: "Blue Light", value: "blue-light" },
-    { label: "Blue Dark", value: "blue-dark" },
-    { label: "Orange Light", value: "orange-light" },
-    { label: "Orange Dark", value: "orange-dark" },
-  ];
 
   const handleThemeButton = (theme: string) => {
     setTheme(theme);
     window.location.reload();
   };
+
+  const handleCursorButton = (caret: "bar" | "block" | "underline") => {
+    const localSettings = localStorage.getItem("settings");
+
+    if (localSettings) {
+      const updatedSettings: SettingsType = JSON.parse(localSettings);
+
+      if (caret === "block" || caret === "underline" || caret === "bar") {
+        updatedSettings.caret = caret;
+        localStorage.setItem("settings", JSON.stringify(updatedSettings));
+        setLocalSettings(updatedSettings);
+        // TODO toast notification for caret change
+      } else {
+        console.error("Invalid caret value");
+      }
+    } else if (!localSettings) {
+      const newSettings: SettingsType = {
+        version: versionNumber,
+        isShowChart: true,
+        caret: "block",
+      };
+
+      localStorage.setItem("settings", JSON.stringify(newSettings));
+    }
+  };
+
+  useEffect(() => {
+    const getVersion = async () => {
+      try {
+        const response = await fetch("/version.json");
+        const { version: localVersion } = await response.json();
+        setVersionNumber(localVersion);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getVersion();
+
+    const localStorageTheme = localStorage.getItem("theme");
+    if (localStorageTheme) setLocalTheme(localStorageTheme);
+
+    const localSettings = localStorage.getItem("settings");
+    if (localSettings) setLocalSettings(JSON.parse(localSettings));
+  }, []);
 
   return (
     <div className="flex h-dvh w-dvw items-center justify-center">
@@ -56,10 +100,9 @@ const SettingPage = () => {
 
         <CardContent>
           <Accordion
-            type="single"
-            collapsible
+            type="multiple"
             className="w-full"
-            defaultValue="theme"
+            defaultValue={["theme", "caret"]}
           >
             <AccordionItem value="theme" defaultChecked={true}>
               <AccordionTrigger>
@@ -68,7 +111,8 @@ const SettingPage = () => {
 
               <AccordionContent>
                 <div className="text-lg">
-                  <div className="flex w-full flex-shrink gap-2">
+                  {/* TODO: stack same colours in the same column */}
+                  <div className="flex w-full gap-2">
                     {themeButtons.map((button) => (
                       <Button
                         key={button.value}
@@ -79,6 +123,48 @@ const SettingPage = () => {
                         {button.label}
                       </Button>
                     ))}
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="caret">
+              <AccordionTrigger>
+                <span className="text-xl">Caret</span>
+              </AccordionTrigger>
+
+              <AccordionContent>
+                <div className="text-lg">
+                  <div className="flex w-full gap-2">
+                    <Button
+                      className="rounded-sm"
+                      onClick={() => {
+                        handleCursorButton("block");
+                      }}
+                      disabled={localSettings.caret === "block"}
+                    >
+                      {"Block ( █ )"}
+                    </Button>
+
+                    <Button
+                      className="rounded-sm"
+                      onClick={() => {
+                        handleCursorButton("bar");
+                      }}
+                      disabled={localSettings.caret === "bar"}
+                    >
+                      {"Bar ( | )"}
+                    </Button>
+
+                    <Button
+                      className="rounded-sm"
+                      onClick={() => {
+                        handleCursorButton("underline");
+                      }}
+                      disabled={localSettings.caret === "underline"}
+                    >
+                      {"Underline ( _ )"}
+                    </Button>
                   </div>
                 </div>
               </AccordionContent>

20f27596152af9aeed246193a7dfbd281548ebc9
LeightonGuang <leighton.guang@icloud.com>
2025-03-04 19:42:44 +0000

    Feat: Support multiple caret styles

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 interface Props {
   char: string;
   isCorrect: boolean | undefined;
@@ -7,12 +9,21 @@ interface Props {
 }
 
 const Letter = ({ char, isCorrect, isActive }: Props) => {
+  const [caret, setCaret] = useState<string>("block");
+
+  useEffect(() => {
+    const localSettings = localStorage.getItem("settings");
+    if (localSettings) setCaret(JSON.parse(localSettings).caret);
+  }, []);
+
+  // TODO blinking caret
+
   return (
     <div
       className={`relative ${isCorrect === undefined && "text-muted-foreground"} ${isCorrect === true && "text-foreground"} ${isCorrect === false && "text-destructive"} `}
     >
       <span
-        className={`absolute inset-0 ${isActive && "bg-input opacity-50"}`}
+        className={`${isActive ? "absolute" : "hidden"} ${caret === "block" && "inset-0 bg-input opacity-50"} ${caret === "underline" && "h-full w-full border-b-2 border-input"} ${caret === "bar" && "h-full w-full border-l border-input"}`}
       />
       {char}
     </div>

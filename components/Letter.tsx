1b52ebb68be7030e49502b5c5681074810874563
LeightonGuang <leighton.guang@icloud.com>
2025-03-08 02:44:51 +0000

    Fix: Default caret to "block" if missing in settings

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -21,7 +21,7 @@ const Letter = ({ char, isCorrect, isActive }: Props) => {
       className={`relative font-mono ${isCorrect === undefined && "text-muted-foreground"} ${isCorrect === true && "text-foreground"} ${isCorrect === false && "text-destructive"} `}
     >
       <span
-        className={`${isActive ? "absolute" : "hidden"} ${caret === "block" && "inset-0 bg-input opacity-50"} ${caret === "bar" && "h-full w-full border-l border-foreground"} ${caret === "underline" && "h-full w-full border-b-2 border-foreground"}`}
+        className={`${isActive ? "absolute" : "hidden"} ${!caret && "inset-0 bg-input opacity-50"} ${caret === "block" && "inset-0 bg-input opacity-50"} ${caret === "bar" && "h-full w-full border-l border-foreground"} ${caret === "underline" && "h-full w-full border-b-2 border-foreground"}`}
       />
       {char}
     </div>
